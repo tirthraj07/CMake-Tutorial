5f5f1a22569f345285ce7917d523d328cfd42f99
Tirthraj Mahajan <39341897+tirthraj07@users.noreply.github.com>
2024-11-14 01:42:53 +0530

    05-example

---
+++ build/CMakeFiles/HttpWebClient.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HttpWebClient.dir/main.cpp.o"
+  "CMakeFiles/HttpWebClient.dir/main.cpp.o.d"
+  "HttpWebClient"
+  "HttpWebClient.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HttpWebClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/HttpWebClient.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for HttpWebClient.
+# This may be replaced when dependencies are built.
